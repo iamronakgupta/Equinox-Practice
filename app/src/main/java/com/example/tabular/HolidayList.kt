61b68f540720f93a7d48bd1ee80c0c69af54969a
Ronak <iamronakgupta@gmail.com>
2021-04-23 20:41:31 +0530

    "second commit"

---
+++ app/src/main/java/com/example/tabular/HolidayList.kt
@@ -0,0 +1,4 @@
+package com.example.tabular
+
+class HolidayList {
+}
---
+++ app/src/main/java/com/example/tabular/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.tabular;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toolbar;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView textView = (TextView) findViewById(R.id.text);
+// ...
+
+// Instantiate the RequestQueue.
+
+        String url = "https://date.nager.at/api/v2/PublicHolidays/2017/AT";
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        textView.setText("Response: " + response.toString());
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+
+                    }
+                });
+
+// Access the RequestQueue through your singleton class.
+
+    }
+
+    private void setSupportActionBar(Toolbar toolbar1) {
+    }
+}
