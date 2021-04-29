051d51ed614529024559f363b8b69734e940ea94
Ronak <iamronakgupta@gmail.com>
2021-04-29 23:41:33 +0530

    "api commit"

--- app/src/main/java/com/example/tabular/MainActivity.java
+++ app/src/main/java/com/example/tabular/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView view;
     RecyclerView.Adapter adapter;
     RecyclerView.LayoutManager manager;
-    ArrayList<ExampleItem> arrayList;
+    ArrayList<ExampleItem> arrayList=new ArrayList<>();
 
 
     @Override
@@ -38,36 +38,44 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         view=findViewById(R.id.recylerview);
 
-        arrayList=new ArrayList<>();
-        arrayList.add(new ExampleItem("new year","1 jan"));
-        arrayList.add(new ExampleItem("new year","1 jan"));
-        arrayList.add(new ExampleItem("new year","1 jan"));
-        arrayList.add(new ExampleItem("new year","1 jan"));
+
+
 
         RequestQueue queue = Volley.newRequestQueue(this);
         JsonArrayRequest arrayRequest=new JsonArrayRequest(Request.Method.GET,
                 "https://date.nager.at/api/v2/PublicHolidays/2017/AT",
                null , new Response.Listener<JSONArray>( ) {
             @Override
-            public void onResponse(JSONArray response) {
+
+            public void onResponse(JSONArray response) throws ArrayIndexOutOfBoundsException {
 
 
                 for(int i=0;i<response.length();i++){
+
                     try {
                         JSONObject jsonObject=response.getJSONObject(i);
                         Log.d("Holiday","on Response "+jsonObject.getString("name") +jsonObject.getString("date"));
 
-
                         arrayList.add(new ExampleItem(jsonObject.getString("name"),jsonObject.getString("date")));
 
+
                     } catch (JSONException e) {
-                        e.printStackTrace( );
+                        e.printStackTrace();
                     }
-                    Toast.makeText(MainActivity.this,arrayList.get(i).getOcassion(),Toast.LENGTH_SHORT).show();
+                    catch (ArrayIndexOutOfBoundsException e){
+                        e.printStackTrace();
+                        Log.d("Error","Out of Bound");
+
+                    }
+
+
+//                    Toast.makeText(MainActivity.this,arrayList.get(i).getOcassion(),Toast.LENGTH_SHORT).show();
 
                 }
 
+
             }
+
         }, new Response.ErrorListener( ) {
             @Override
             public void onErrorResponse(VolleyError error) {
@@ -76,10 +84,11 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-        view=findViewById(R.id.recylerview);
+        arrayList.add(new ExampleItem("first","first"));
+        Log.d("Array","on Response "+arrayList.get(0).getOcassion() +arrayList.get(0).getDate());
         manager=new LinearLayoutManager(this);
-        view.setHasFixedSize(false);
+
+//
         adapter=new CustomAdapter(arrayList);
         view.setLayoutManager(manager);
         view.setAdapter(adapter);
